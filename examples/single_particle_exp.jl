5804980b7c39f1dd359003956a565b1120f4b2f4
Emily de Jong <edejong@caltech.edu>
2023-08-18 13:59:53 -0700

    Finishing single particle example

--- examples/single_particle_exp.jl
+++ examples/single_particle_exp.jl
@@ -1,7 +1,7 @@
 "Test case with a single exponential distribution"
 
 using DifferentialEquations
-#using Plots
+using Plots
 
 using SuperParticles.KernelFunctions
 using SuperParticles.ParticleDistributions
@@ -21,19 +21,76 @@ function rhs!(ddist_moments, dist_moments, p, t)
     end
     # update the information
     update_coal_ints!(p.Nmom, p.kernel_func, p.pdists, p.coal_data)
-    @show p.coal_data.coal_ints
     ddist_moments .= p.coal_data.coal_ints
-    @show ddist_moments
+end
+
+function plot_moments!(sol, p; plt_title="test_moments")
+    Plots.gr()
+    time = sol.t
+    moment_plots = []
+    for i in 1:p.Nmom
+        ploti = plot(time,
+                vcat(sol.u...)[:,i],
+                linewidth=2,
+                xaxis="time [s]",
+                yaxis="M"*string(i-1),
+                label="M_{"*string(i-1)*",1}",
+                xlims=(0, maximum(time)),
+                ylims=(0, 1.2*maximum(vcat(sol.u...)[:,i]))
+            )
+        push!(moment_plots, 
+            ploti
+        )
+    end
+    plot(moment_plots...)
+    savefig("examples/"*plt_title*".png")
+end
+
+function plot_spectra!(sol, p; plt_title="test_spectra", logxrange=(0, 8))
+    Plots.gr()
+    x = 10 .^ (collect(range(logxrange[1], logxrange[2], 100)))
+    r = (x * 3 / 4 / π) .^ (1/3)
+
+    # initial distribution
+    for i=1:p.Ndist
+        update_dist_from_moments!(p.pdists[i], vcat(sol.u...)[1,:])
+    end
+    pinit = plot(r,
+        3*x.*p.pdists[1].(x),
+        linewidth=2,
+        xaxis=:log,
+        yaxis="dV / d(ln r)",
+        xlabel="r",
+        xlim=(minimum(r), maximum(r)),
+        label="Initial, pdist 1"
+    )
+
+    # final distribution
+    for i=1:p.Ndist
+        update_dist_from_moments!(p.pdists[i], vcat(sol.u...)[end,:])
+    end
+    pfin = plot(r,
+        3*x.*p.pdists[1].(x),
+        linewidth=2,
+        xaxis=:log,
+        yaxis="dV / d(ln r)",
+        xlabel="r",
+        xlim=(minimum(r), maximum(r)),
+        label="Final, pdist 1"
+    )
+
+    plot(pinit, pfin)
+    savefig("examples/"*plt_title*".png")
 end
 
 function main()
     T_end = 1.0
-    coalescence_coeff = 1e-4
+    coalescence_coeff = 1e-3
     kernel = LinearKernelFunction(coalescence_coeff)
 
     # Initial condition 
     Ndist = 1
-    particle_number = [10.0]
+    particle_number = [100.0]
     mass_scale = [100.0]
     Nmom = 2
 
@@ -48,14 +105,15 @@ function main()
     end
 
     # Set up ODE information
-    ddist_moments = zeros(FT, Ndist, Nmom)
     coal_data = initialize_coalescence_data(Ndist, Nmom)
     p = (Ndist=Ndist, Nmom=Nmom, pdists=pdists, kernel_func=kernel, coal_data=coal_data)
 
     tspan = (0.0, T_end)
     prob = ODEProblem(rhs!, dist_moments, tspan, p; progress=true)
     sol = solve(prob, Tsit5(), reltol=tol, abstol=tol)
-
+    @show sol.u
+    plot_moments!(sol, p; plt_title="single_particle_exp_moments")
+    plot_spectra!(sol, p; plt_title="single_particle_exp_spectra")
 end
 
 @time main()
